467c2eafe82eb57edac29df8eefdc2666163eea9
Chinnutzz <72239294+Chinnutzz@users.noreply.github.com>
2023-09-25 20:14:45 -0300

    Update WTS _Liathna_Kingdom.js

--- WTS _Liathna_Kingdom.js
+++ WTS _Liathna_Kingdom.js
@@ -1,3 +1,16 @@
-window.onload = function () {
-    alert("Esta é uma ficha personalizada no Roll20 com JavaScript externo!");
-};
+// Função para calcular os PVs com base no Nível e Físico
+function calcularPVs() {
+    // Obtenha os valores de Nível e Físico
+    var nivel = parseInt(document.getElementById("nivel").value, 10);
+    var fisico = parseInt(document.getElementById("fisico").value, 10);
+
+    // Realize o cálculo dos PVs
+    var pvs = (10 + fisico) * nivel;
+
+    // Atualize o campo "pvMax" com o valor calculado
+    document.getElementById("pvMax").value = pvs;
+}
+
+// Adicione um evento que chama a função calcularPVs quando o Nível ou Físico mudar
+document.getElementById("nivel").addEventListener("change", calcularPVs);
+document.getElementById("fisico").addEventListener("change", calcularPVs);
